50e170171203d33a4f4d36d35abcb5f7da5752a0
Raaba <raaba21@gmail.com>
2024-04-08 19:47:42 -0400

    removed excessive projects and added another slot for full stack

--- src/assets/components/Projects/index.jsx
+++ src/assets/components/Projects/index.jsx
@@ -25,22 +25,7 @@ const Project = () => {
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
                         </div>
                     </div>
-                    <div className="single-project-container">
-                        <Link to="/projects/1" className="image-container"><img src={placeholder} alt="" /></Link>
-                        <div className="content">
-                            <h2>This is a tomato</h2>
-                            <h3>Technologies Used: </h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
-                        </div>
-                    </div>
-                    <div className="single-project-container">
-                        <Link to="/projects/1" className="image-container"><img src={placeholder} alt="" /></Link>
-                        <div className="content">
-                            <h2>This is a tomato</h2>
-                            <h3>Technologies Used: </h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
-                        </div>
-                    </div>
+                    
 
                 </div>
 
@@ -68,23 +53,33 @@ const Project = () => {
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
                         </div>
                     </div>
-                    <div className="single-project-container">
-                        <Link to="/projects/1" className="image-container"><img src={placeholder} alt="" /></Link>
-                        <div className="content">
-                            <h2>This is a tomato</h2>
-                            <h3>Technologies Used: </h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
-                        </div>
-                    </div>
-                    <div className="single-project-container">
-                        <Link to="/projects/1" className="image-container"><img src={placeholder} alt="" /></Link>
-                        <div className="content">
-                            <h2>This is a tomato</h2>
-                            <h3>Technologies Used: </h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
-                        </div>
-                    </div>
+                    
+                </div>
 
+
+
+            </div>
+            <div className="backend-container">
+                <h1>FULL-STACK Projects</h1>
+
+                <div className="project-container">
+                    <div className="single-project-container">
+                        <Link to="/projects/1" className="image-container"><img src={placeholder} alt="" /></Link>
+                        <div className="content">
+                            <h2>This is a tomato</h2>
+                            <h3>Technologies Used: </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
+                        </div>
+                    </div>
+                    <div className="single-project-container">
+                        <Link to="/projects/1" className="image-container"><img src={placeholder} alt="" /></Link>
+                        <div className="content">
+                            <h2>This is a tomato</h2>
+                            <h3>Technologies Used: </h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptate eligendi nihil saepe repellendus, ipsam possimus reprehenderit consequuntur labore, alias qui tenetur tempore! Sed a reprehenderit ut commodi optio modi?</p>
+                        </div>
+                    </div>
+                    
                 </div>
 
 

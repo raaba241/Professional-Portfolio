f7c4a2a913fa99e4a7951be29d2115520e773afb
Raaba <raaba21@gmail.com>
2024-04-08 07:11:58 -0400

    added a profile image

--- src/assets/components/Banner/index.jsx
+++ src/assets/components/Banner/index.jsx
@@ -2,17 +2,24 @@ import './style.css'
 import githubIcon from "../../icons/github.png"
 import linkedInIcon from "../../icons/linkedin.png"
 import gmailIcon from "../../icons/gmail.png"
+import profileImg from "../../images/profilePictureWorking.png"
 const Banner = () => {
     return (
         <div className="intro-para">
-            <p>Hi, <br></br> Im <span className='name'>Raaba Ahmad</span>. Im a fullstack web developer in Toronto, ON. I love learning the inner workings of things and always excited to trying something new. I am full of passion and enthusiam when it comes to programming. I love anime.</p>
+            <div className='banner-content'>
+                <p>Hi, <br></br> Im <span className='name'>Raaba Ahmad</span>. Im a fullstack web developer in Toronto, ON. I love learning the inner workings of things and always excited to trying something new. I am full of passion and enthusiam when it comes to programming. I love anime.</p>
 
-            <a href="https://github.com/raaba241"><img src={githubIcon} alt="github logo" /></a>
-            <a href="https://www.linkedin.com/in/ahmad-raaba-07937129a/"><img src={linkedInIcon} alt="linkedIn logo" /></a>
-            <a href="https://github.com/raaba241"><img src={gmailIcon} alt="github logo" /></a>
+                <a href="https://github.com/raaba241"><img src={githubIcon} alt="github logo" /></a>
+                <a href="https://www.linkedin.com/in/ahmad-raaba-07937129a/"><img src={linkedInIcon} alt="linkedIn logo" /></a>
+                <a href="https://github.com/raaba241"><img src={gmailIcon} alt="github logo" /></a>
+            </div>
+
+            <div className='profilePic'>
+                <img src={profileImg} alt="" />
+            </div>
         </div>
 
-        
+
     )
 }
 
